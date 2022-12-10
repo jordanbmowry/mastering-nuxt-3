5e38d8f705c982773546369217610351dda18143
jordanbmowry <jmowry@taponit.com>
2022-12-10 16:01:10 -0600

    add route middleware

--- pages/course.vue
+++ pages/course.vue
@@ -1,5 +1,5 @@
 <script setup>
-const { chapters } = useCourse();
+const { chapters, title } = useCourse();
 const resetError = async (error) => {
   throw createError({
     fatal: true,
@@ -9,54 +9,55 @@ const resetError = async (error) => {
 </script>
 
 <template>
-  <div class="prose mb-12">
-    <h1>
-      <span class="font-medium">
-        Course:
-        <span class="font-bold">Mastering Nuxt 3</span>
-      </span>
-    </h1>
-  </div>
-
-  <div class="flex flex-row justify-center flex-grow">
-    <div
-      class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
-    >
-      <h3>Chapters</h3>
-      <div
-        class="space-y-1 mb-4 flex flex-col"
-        v-for="chapter in chapters"
-        :key="chapter.slug"
-      >
-        <h4>{{ chapter.title }}</h4>
-        <NuxtLink
-          v-for="(lesson, index) in chapter.lessons"
-          :key="lesson.slug"
-          class="flex flex-row space-x-1 no-underline prose-sm font-normal"
-          :to="lesson.path"
-        >
-          <span class="text-gray-500">{{ index + 1 }}.</span>
-          <span>{{ lesson.title }}</span>
-        </NuxtLink>
-      </div>
+  <div>
+    <div class="prose mb-12">
+      <h1>
+        <span class="font-medium">
+          <span class="font-bold">{{ title }}</span>
+        </span>
+      </h1>
     </div>
 
-    <div class="prose p-12 bg-white rounded-md w-[65ch]">
-      <NuxtErrorBoundary>
-        <NuxtPage />
-        <template #error="{ error }">
-          <p>
-            Oh no, something went wrong with the lesson!
-            <code>{{ error }}</code>
-          </p>
-          <button
-            class="hover:cursor-pointer bg-gray-500 text-white font-bold py-1 px-3 rounded"
-            @click="resetError(error)"
+    <div class="flex flex-row justify-center flex-grow">
+      <div
+        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
+      >
+        <h3>Chapters</h3>
+        <div
+          class="space-y-1 mb-4 flex flex-col"
+          v-for="chapter in chapters"
+          :key="chapter.slug"
+        >
+          <h4>{{ chapter.title }}</h4>
+          <NuxtLink
+            v-for="(lesson, index) in chapter.lessons"
+            :key="lesson.slug"
+            class="flex flex-row space-x-1 no-underline prose-sm font-normal"
+            :to="lesson.path"
           >
-            Reset
-          </button>
-        </template>
-      </NuxtErrorBoundary>
+            <span class="text-gray-500">{{ index + 1 }}.</span>
+            <span>{{ lesson.title }}</span>
+          </NuxtLink>
+        </div>
+      </div>
+
+      <div class="prose p-12 bg-white rounded-md w-[65ch]">
+        <NuxtErrorBoundary>
+          <NuxtPage />
+          <template #error="{ error }">
+            <p>
+              Oh no, something went wrong with the lesson!
+              <code>{{ error }}</code>
+            </p>
+            <button
+              class="hover:cursor-pointer bg-gray-500 text-white font-bold py-1 px-3 rounded"
+              @click="resetError(error)"
+            >
+              Reset
+            </button>
+          </template>
+        </NuxtErrorBoundary>
+      </div>
     </div>
   </div>
 </template>
--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -3,32 +3,38 @@ const course = useCourse();
 const route = useRoute();
 
 definePageMeta({
-  validate({ params }) {
-    const course = useCourse();
+  middleware: [
+    function ({ params }, from) {
+      const course = useCourse();
 
-    const chapter = course.chapters.find(
-      (chapter) => chapter.slug === params.chapterSlug
-    );
+      const chapter = course.chapters.find(
+        (chapter) => chapter.slug === params.chapterSlug
+      );
 
-    if (!chapter) {
-      return createError({
-        statusCode: 404,
-        message: 'Chapter not found',
-      });
-    }
+      if (!chapter) {
+        return abortNavigation(
+          abortNavigation(
+            createError({
+              statusCode: 404,
+              message: 'Chapter not found',
+            })
+          )
+        );
+      }
 
-    const lesson = chapter.lessons.find(
-      (lesson) => lesson.slug === params.lessonSlug
-    );
+      const lesson = chapter.lessons.find(
+        (lesson) => lesson.slug === params.lessonSlug
+      );
 
-    if (!lesson) {
-      return createError({
-        statusCode: 404,
-        message: 'Lesson not found',
-      });
-    }
-    return true;
-  },
+      if (!lesson) {
+        return createError({
+          statusCode: 404,
+          message: 'Lesson not found',
+        });
+      }
+    },
+    'auth',
+  ],
 });
 
 if (route.params.lessonSlug === '3-typing-component-events') {
