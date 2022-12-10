6a8fe1c2c38dbaff84e9fe886237b389d71ba9eb
jordanbmowry <jmowry@taponit.com>
2022-12-10 12:28:26 -0600

    recover from error

--- pages/course.vue
+++ pages/course.vue
@@ -1,46 +1,62 @@
 <script setup>
 const { chapters } = useCourse();
+const resetError = async (error) => {
+  throw createError({
+    fatal: true,
+    message: 'Fatal error',
+  });
+};
 </script>
 
 <template>
-  <div
-    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
-  >
-    <div class="prose mb-12">
-      <h1>
-        <span class="font-medium">
-          Course:
-          <span class="font-bold">Mastering Nuxt 3</span>
-        </span>
-      </h1>
+  <div class="prose mb-12">
+    <h1>
+      <span class="font-medium">
+        Course:
+        <span class="font-bold">Mastering Nuxt 3</span>
+      </span>
+    </h1>
+  </div>
+
+  <div class="flex flex-row justify-center flex-grow">
+    <div
+      class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
+    >
+      <h3>Chapters</h3>
+      <div
+        class="space-y-1 mb-4 flex flex-col"
+        v-for="chapter in chapters"
+        :key="chapter.slug"
+      >
+        <h4>{{ chapter.title }}</h4>
+        <NuxtLink
+          v-for="(lesson, index) in chapter.lessons"
+          :key="lesson.slug"
+          class="flex flex-row space-x-1 no-underline prose-sm font-normal"
+          :to="lesson.path"
+        >
+          <span class="text-gray-500">{{ index + 1 }}.</span>
+          <span>{{ lesson.title }}</span>
+        </NuxtLink>
+      </div>
     </div>
 
-    <div class="flex flex-row justify-center flex-grow">
-      <div
-        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
-      >
-        <h3>Chapters</h3>
-        <div
-          class="space-y-1 mb-4 flex flex-col"
-          v-for="chapter in chapters"
-          :key="chapter.slug"
-        >
-          <h4>{{ chapter.title }}</h4>
-          <NuxtLink
-            v-for="(lesson, index) in chapter.lessons"
-            :key="lesson.slug"
-            class="flex flex-row space-x-1 no-underline prose-sm font-normal"
-            :to="lesson.path"
-          >
-            <span class="text-gray-500">{{ index + 1 }}.</span>
-            <span>{{ lesson.title }}</span>
-          </NuxtLink>
-        </div>
-      </div>
-
-      <div class="prose p-12 bg-white rounded-md w-[65ch]">
+    <div class="prose p-12 bg-white rounded-md w-[65ch]">
+      <NuxtErrorBoundary>
         <NuxtPage />
-      </div>
+        <template #error="{ error }">
+          <p>
+            Oh no, something went wrong with the lesson!
+            <code>{{ error }}</code>
+          </p>
+          <button
+            class="hover:cursor-pointer bg-gray-500 text-white font-bold py-1 px-3 rounded"
+            @click="resetError(error)"
+          >
+            Reset
+          </button>
+        </template>
+      </NuxtErrorBoundary>
     </div>
   </div>
 </template>
