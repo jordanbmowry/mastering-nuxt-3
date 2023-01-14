5ce5403402902ac5caffe7fdd0ff53646f5e9bca
jordanbmowry <jmowry@taponit.com>
2023-01-14 12:42:54 -0600

    modified login page

--- pages/login.vue
+++ pages/login.vue
@@ -11,12 +11,27 @@
 </template>
 
 <script setup lang="ts">
+import { userInfo } from 'os';
+import { queuePostFlushCb } from 'vue';
+
 const { title } = useCourse();
+const { query } = useRoute();
 const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+
+watchEffect(async () => {
+  if (user.value) {
+    await navigateTo(query.redirectTo as string, {
+      replace: true,
+    });
+  }
+});
 
 const login = async () => {
+  const redirectTo = `${window.location.origin}${query.redirectTo}`;
   const { error } = await supabase.auth.signInWithOAuth({
     provider: 'github',
+    options: { redirectTo },
   });
 
   if (error) {
